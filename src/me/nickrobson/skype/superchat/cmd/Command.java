b84fb9eb9897452822248ffc8850074441284b7e
nickrobson <robson.nicholas1@gmail.com>
2015-10-10 12:30:40 +1100

    Remove unused Command#setBold method since MessageBuilder has a method now

--- src/me/nickrobson/skype/superchat/cmd/Command.java
+++ src/me/nickrobson/skype/superchat/cmd/Command.java
@@ -3,7 +3,6 @@ package me.nickrobson.skype.superchat.cmd;
 import me.nickrobson.skype.superchat.SuperChatListener;
 import xyz.gghost.jskype.Group;
 import xyz.gghost.jskype.message.Message;
-import xyz.gghost.jskype.message.MessageBuilder;
 import xyz.gghost.jskype.user.GroupUser;
 
 public interface Command {
@@ -22,15 +21,6 @@ public interface Command {
 
 	default void sendMessage(Group group, String message, boolean encode) {
 		SuperChatListener.sendMessage(group, message, encode);
-	}
-	
-	default void setBold(MessageBuilder builder, boolean bold) {
-		try {
-			builder.getClass().getField("bold").setAccessible(true);
-			builder.getClass().getField("bold").set(builder, bold);
-		} catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException ex) {
-			ex.printStackTrace();
-		}
 	}
 	
 }
