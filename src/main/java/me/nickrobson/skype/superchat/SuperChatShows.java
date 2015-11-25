4012cd194bcb0c97801dc51f123e31e2aa0dff7f
nickrobson <robson.nicholas1@gmail.com>
2015-11-25 23:15:54 +1100

    Add Empire as a watched show.

--- src/main/java/me/nickrobson/skype/superchat/SuperChatShows.java
+++ src/main/java/me/nickrobson/skype/superchat/SuperChatShows.java
@@ -11,38 +11,39 @@ public class SuperChatShows {
 	public static final List<Show> TRACKED_SHOWS = new ArrayList<>();
 	public static final Map<String, Show> SHOWS_BY_NAME = new HashMap<>();
 	public static final Pattern EPISODE_PATTERN = Pattern.compile("S[1-9][0-9]?E[1-9][0-9]?");
-	
-	public static void setup() {
-		if (TRACKED_SHOWS.size() > 0)
-			return;
-		// format:
-		// you only need the first two strings ("Display Name" and "main"), the others are optional!
-		// Please keep the "main" string to only contain lowercase english letters (as it is used for file names)!
-		// register(new Show("Display Name", "main", "other_name", "any_other_name", "blah"));
-		register(new Show("Arrow", "Wednesday", "arrow", "thearrow"));
-		register(new Show("The Flash", "Tuesday", "flash", "theflash"));
-		register(new Show("Agents of SHIELD", "Tuesday", "aos", "agents"));
-		register(new Show("Agent Carter", null, "ac", "agentcarter"));
-		register(new Show("Jessica Jones", "Netflix", "jj", "jessicajones"));
-		register(new Show("Daredevil", "Netflix", "dd", "daredevil"));
-		register(new Show("Supergirl", "Monday", "sg", "supergirl"));
-		register(new Show("Gotham", "Monday", "gotham", "goth"));
-		register(new Show("Constantine", null, "const", "constantine"));
-		register(new Show("Narcos", "Netflix", "narcos", "narc"));
-		register(new Show("Mr. Robot", null, "robot", "mrrobot"));
-		register(new Show("sense8", "Netflix", "sensate", "sense8", "s8"));
-		register(new Show("SleepyHollow", "Thursday", "sho", "sleho"));
-		register(new Show("Grimm", "Friday", "grimm", "gri"));
-		register(new Show("The Walking Dead", "Monday", "twd", "walkingdead"));
-		register(new Show("Heroes",	null, "heroes", "her"));
-		register(new Show("Doctor Who", "Sunday", "dw", "doctorwho"));
-		register(new Show("The Tomorrow People", null, "ttp", "tmp"));
-		register(new Show("The Messengers", null, "tmsg", "messengers"));
-		register(new Show("Limitless", "Tuesday", "limitless", "lmtl"));
+
+    public static void setup() {
+        if (TRACKED_SHOWS.size() > 0)
+            return;
+        // Format:
+        //     register(new Show("Display Name", "main", "other_name", "any_other_name", "blah"));
+        // Note:
+        //     You only need the first two strings ("Display Name" and "main"), the others are optional!
+        //     Please keep the "main" string to only contain lowercase english letters (as it is used for file names)!
+        register(new Show("Arrow", "Wednesday", "arrow", "thearrow"));
+        register(new Show("The Flash", "Tuesday", "flash", "theflash"));
+        register(new Show("Agents of SHIELD", "Tuesday", "aos", "agents"));
+        register(new Show("Agent Carter", null, "ac", "agentcarter"));
+        register(new Show("Jessica Jones", "Netflix", "jj", "jessicajones"));
+        register(new Show("Daredevil", "Netflix", "dd", "daredevil"));
+        register(new Show("Supergirl", "Monday", "sg", "supergirl"));
+        register(new Show("Gotham", "Monday", "gotham", "goth"));
+        register(new Show("Constantine", null, "const", "constantine"));
+        register(new Show("Narcos", "Netflix", "narcos", "narc"));
+        register(new Show("Mr. Robot", null, "robot", "mrrobot"));
+        register(new Show("sense8", "Netflix", "sensate", "sense8", "s8"));
+        register(new Show("SleepyHollow", "Thursday", "sho", "sleho"));
+        register(new Show("Grimm", "Friday", "grimm", "gri"));
+        register(new Show("The Walking Dead", "Monday", "twd", "walkingdead"));
+        register(new Show("Heroes", null, "heroes", "her"));
+        register(new Show("Doctor Who", "Sunday", "dw", "doctorwho"));
+        register(new Show("The Tomorrow People", null, "ttp", "tmp"));
+        register(new Show("The Messengers", null, "tmsg", "messengers"));
+        register(new Show("Limitless", "Tuesday", "limitless", "lmtl"));
         register(new Show("iZombie", "Tuesday", "izombie", "izo"));
         register(new Show("Blindspot", "Monday", "blindspot", "blind", "bli"));
-        register(new Show("Empire", null, "empire", "emp"));
-	}
+        register(new Show("Empire", "Wednesday", "empire", "emp"));
+    }
 	
 	public static void register(Show show) {
 		TRACKED_SHOWS.add(show);
