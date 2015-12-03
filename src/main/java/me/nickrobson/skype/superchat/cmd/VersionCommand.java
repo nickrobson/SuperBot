bf6a8d55824e1e0b5b380502fb1ce341f6d037e2
Nick Robson <robson.nicholas1@gmail.com>
2015-12-04 03:27:51 +1100

    nope, italics it is

--- src/main/java/me/nickrobson/skype/superchat/cmd/VersionCommand.java
+++ src/main/java/me/nickrobson/skype/superchat/cmd/VersionCommand.java
@@ -35,7 +35,7 @@ public class VersionCommand implements Command {
             String msg = SuperChatController.GIT_COMMIT_MESSAGE[i];
             String author = SuperChatController.GIT_COMMIT_AUTHORS[i];
             String txt = new MessageBuilder()
-                    .underline(true).text(author + " ").underline(false)
+                    .italic(true).text(author + " ").italic(false)
                     .text(msg)
                     .italic(true).text(" (" + id + ")").italic(false)
                     .build();
