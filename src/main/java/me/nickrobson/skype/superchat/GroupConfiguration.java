e19aaea9cfa2adf08ec4549795094df811310512
Nick Robson <robson.nicholas1@gmail.com>
2016-01-01 17:12:17 +0800

    Begin implementing saving of GroupConfigurations to file

--- src/main/java/me/nickrobson/skype/superchat/GroupConfiguration.java
+++ src/main/java/me/nickrobson/skype/superchat/GroupConfiguration.java
@@ -1,9 +1,11 @@
 package me.nickrobson.skype.superchat;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -18,7 +20,10 @@ public class GroupConfiguration {
     private boolean           showJoinMessage = false;
     private boolean           showEdited      = false;
 
+    private File              file;
+
     public GroupConfiguration(File file) {
+        this.file = file;
         try (BufferedReader reader = Files.newBufferedReader(file.toPath())) {
             String line;
             while ((line = reader.readLine()) != null) {
@@ -35,6 +40,38 @@ public class GroupConfiguration {
                 }
             }
             reader.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void save() {
+        try {
+            BufferedWriter writer = Files.newBufferedWriter(file.toPath(), StandardOpenOption.CREATE);
+            if (groupId != null) {
+                writer.write("groupId=" + groupId);
+                writer.newLine();
+            }
+            if (showEdited) {
+                writer.write("show-edited");
+                writer.newLine();
+            }
+            if (everythingOn) {
+                writer.write("everything-on");
+                writer.newLine();
+            }
+            if (showJoinMessage) {
+                writer.write("join-message");
+                writer.newLine();
+            }
+            enabledCommands.forEach(c -> {
+                try {
+                    writer.write("cmd." + c + "=true");
+                    writer.newLine();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            });
         } catch (IOException ex) {
             ex.printStackTrace();
         }
