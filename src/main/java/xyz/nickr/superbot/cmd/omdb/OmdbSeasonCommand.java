f178521f6fe05f29e884572f1b54642ccc41ab36
Nick Robson <robson.nicholas1@gmail.com>
2016-04-10 19:22:11 +1000

    Code tags should not be taken into account with line length

--- src/main/java/xyz/nickr/superbot/cmd/omdb/OmdbSeasonCommand.java
+++ src/main/java/xyz/nickr/superbot/cmd/omdb/OmdbSeasonCommand.java
@@ -34,7 +34,7 @@ public class OmdbSeasonCommand implements Command {
     }
 
     String toString(Sys sys, SeasonEpisodeResult episode) {
-        return sys.message().code(true).escaped("E" + episode.episode + " (" + episode.imdbRating + "): " + episode.title).build();
+        return sys.message().escaped("E" + episode.episode + " (" + episode.imdbRating + "): " + episode.title).build();
     }
 
     @Override
@@ -45,12 +45,13 @@ public class OmdbSeasonCommand implements Command {
             MessageBuilder<?> mb = sys.message();
             if (JavaOMDB.IMDB_ID_PATTERN.matcher(args[0]).matches()) {
                 SeasonResult season = SuperBotController.OMDB.seasonById(args[0], args[1]);
-                mb.escaped(season.title + ", season " + args[1] + ": ");
+                mb.bold(true).escaped(season.title).bold(false).escaped(", season " + args[1] + ":");
                 List<SeasonEpisodeResult> episodes = Arrays.asList(season.episodes);
                 List<String> infos = episodes.stream().map(s -> toString(sys, s)).collect(Collectors.toList());
                 boolean cols = sys.columns();
                 int rows = cols ? episodes.size() / 2 + episodes.size() % 2 : episodes.size();
                 int maxLen = infos.subList(0, rows).stream().mapToInt(s -> s.length()).max().orElse(0);
+                mb.code(true);
                 for (int i = 0; i < rows; i++) {
                     String s = pad(infos.get(i), maxLen);
                     if (cols && episodes.size() > i + rows) {
@@ -58,6 +59,7 @@ public class OmdbSeasonCommand implements Command {
                     }
                     mb.newLine().raw(s);
                 }
+                mb.code(false);
             } else {
                 mb.escaped("Invalid IMDB ID (" + args[0] + ")");
             }
