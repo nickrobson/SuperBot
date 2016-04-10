567ac4f978555b0e127017f8ddccab325c9a7728
Nick Robson <robson.nicholas1@gmail.com>
2016-04-10 10:59:03 +1000

    Columns for seasons

--- src/main/java/xyz/nickr/superbot/cmd/omdb/OmdbSeasonCommand.java
+++ src/main/java/xyz/nickr/superbot/cmd/omdb/OmdbSeasonCommand.java
@@ -1,5 +1,8 @@
 package xyz.nickr.superbot.cmd.omdb;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import xyz.nickr.jomdb.JavaOMDB;
 import xyz.nickr.jomdb.model.SeasonEpisodeResult;
 import xyz.nickr.jomdb.model.SeasonResult;
@@ -23,6 +26,12 @@ public class OmdbSeasonCommand implements Command {
         return new String[]{ "[imdbId] [season]", "get season information" };
     }
 
+    private String pad(String s, int len) {
+        while (s.length() < len)
+            s += " ";
+        return s;
+    }
+
     @Override
     public void exec(Sys sys, User user, Group group, String used, String[] args, Message message) {
         if (args.length < 2) {
@@ -32,9 +41,20 @@ public class OmdbSeasonCommand implements Command {
             if (JavaOMDB.IMDB_ID_PATTERN.matcher(args[0]).matches()) {
                 SeasonResult season = SuperBotController.OMDB.seasonById(args[0], args[1]);
                 mb.escaped(season.title + ", season " + args[1] + ": ");
+                List<String> infos = new LinkedList<>();
                 for (SeasonEpisodeResult episode : season) {
-                    mb.newLine().bold(true).escaped("E" + episode.episode + " (" + episode.imdbRating + ")").bold(false);
-                    mb.escaped(": " + episode.title);
+                    MessageBuilder<?> emb = sys.message();
+                    emb.bold(true).escaped("E" + episode.episode + " (" + episode.imdbRating + ")").bold(false);
+                    emb.escaped(": " + episode.title);
+                    infos.add(emb.build());
+                }
+                int maxLen = infos.subList(0, sys.columns() ? infos.size() / 2 : infos.size()).stream().mapToInt(s -> s.length()).max().orElse(0);
+                for (int i = 0, j = sys.columns() ? infos.size() / 2 : infos.size(); i < j; i++) {
+                    if (sys.columns() && i >= j/2) {
+                        mb.escaped("   " + infos.get(i));
+                    } else {
+                        mb.newLine().raw(pad(infos.get(i), maxLen));
+                    }
                 }
             } else {
                 mb.escaped("Invalid IMDB ID (" + args[0] + ")");
